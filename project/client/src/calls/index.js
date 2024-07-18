50354c02905f9b4f72bd3905cf76bb23c0f6f6c5
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2024-07-18 23:31:32 +0530

    Updated baseURL in index.js

--- project/client/src/calls/index.js
+++ project/client/src/calls/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-
 export const axiosInstance = axios.create({
     headers : {
-
+        baseURL : 'http://localhost:8081',
         'Content-Type' : 'application/json',
         'authorization' : `Bearer ${localStorage.getItem('token')}`
     }
